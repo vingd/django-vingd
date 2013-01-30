7fb728525b5fa4a01c28e386eec640418f51441d
Andjelko Horvat <comel@vingd.com>
2013-01-30 17:59:15 +0100

    setup.py: version 0.1.1.

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from distutils.core import setup
 
 setup(
     name='django-vingd',
-    version='0.1.0',
+    version='0.1.1',
     description='Django App for Vingd integration',
     long_description=open('README.rst').read(),
     author='Marin Prcela',
